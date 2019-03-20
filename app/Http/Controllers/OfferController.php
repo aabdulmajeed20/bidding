547278d7d9adcd0fcb3ad16fc9439d02bf5f6a01
ahmed-abdullah9 <ahmed01799@gmail.com>
2019-03-20 18:42:36 +0300

    change names

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Offer;
+use App\Provider;
+use App\Bid;
 
 class OfferController extends Controller
 {
 
-    public function addOffer()
+    public function addOffer(Request $request)
     {
+        $provider_id = Session::get('provider_id');
+        $provider = Provider::where('_id', $provider_id)->first();
+
+        $bid = Bid::where('_id', );
+        $offer = new Offer();
+        $offer->price = $request->price;
         
+
     }
     //
 }
