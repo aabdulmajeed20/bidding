531af4781e50460838d6ee918b4b1478886d45dc
Abdulaziz Alshehri <a.alshehri@cbxunit.com>
2019-04-07 12:06:33 +0300

    added failed message

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,6 +4,12 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+          @if (\Session::has('failed'))
+            <div class="alert alert-danger">
+              <p>{{ \Session::get('failed') }}</p>
+            </div><br />
+          @endif
+
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
